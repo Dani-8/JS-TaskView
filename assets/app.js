854f3e99ed8c2321eeb2d5ccfead31caa403c33b
Dani-8 <daniyalw6190@gmail.com>
2025-09-02 00:08:22 +0500

    Updates

--- assets/app.js
+++ assets/app.js
@@ -3,18 +3,71 @@ let tasksCont = document.getElementById("tasks-cont");
 // RENDER A TASK BLOCK
 function renderTask(title, descp, code, result){
     tasksCont.innerHTML += `
-        
-    `
+        <div class="task-block">
+            <h2 class="title">${title}</h2>
+            <p class="desc">${descp}</p>
 
+            <div class="code-block">
+                <h3>Code:</h3>
+                <pre class="code">${code}</pre>
+            </div>
+            <div class="result-block">
+                <h3>Result:</h3>
+                <div class="output">${result}</div>
+            </div>
+        </div>
+    `
 }
 
+// -------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------
+
+// TASK-1: FIND MIN & MAX IN ARRAY
+function findMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return { min, max };
+}
+
+// Example usage:
+const numbers = [5, 2, 9, 1, 5, 6];
+const { min, max } = findMinMax(numbers);
+console.log("Min:", min);
+console.log("Max:", max);
 
 
+const reverseCode = `function findMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
 
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return { min, max };
+}
 
+// Example usage:
+const numbers = [5, 2, 9, 1, 5, 6];
+const { min, max } = findMinMax(numbers);
+console.log("Min:", min);
+console.log("Max:", max);
+`;
 
-
-
+renderTask("Task-1: Find Min & Max", "Find the minimum and maximum values in an array.", reverseCode, `Numbers: [5, 2, 9, 1, 5, 6]<br> Minimum: ${min}<br> Maximum: ${max}`);
 
 
 
