b85d2a565563b63bd935582f35a022c0fb5bdd55
Dani-8 <daniyalw6190@gmail.com>
2025-09-01 23:36:46 +0500

    Updates

---
+++ assets/app.js
@@ -0,0 +1,49 @@
+let tasksCont = document.getElementById("tasks-cont");
+
+// RENDER A TASK BLOCK
+function renderTask(title, descp, code, result){
+    tasksCont.innerHTML += `
+        
+    `
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
